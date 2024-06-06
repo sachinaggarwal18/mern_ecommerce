38e643f3c59f4e25cb315c105d8549f6ce913955
Sachin <raisethebat2020@gmail.com>
2024-06-07 00:19:22 +0530

    some changes-files removed

--- ecommerce-frontend/src/App.tsx
+++ ecommerce-frontend/src/App.tsx
@@ -18,9 +18,11 @@ const Coupon = lazy(() => import("./pages/admin/apps/coupon"));
 const Stopwatch = lazy(() => import("./pages/admin/apps/stopwatch"));
 const Toss = lazy(() => import("./pages/admin/apps/toss"));
 const NewProduct = lazy(() => import("./pages/admin/management/newproduct"));
+
 const ProductManagement = lazy(
   () => import("./pages/admin/management/productmanagement")
 );
+
 const TransactionManagement = lazy(
   () => import("./pages/admin/management/transactionmanagement")
 );
@@ -33,6 +35,36 @@ const App = () => {
         <Route path='/' element={<Home/>} />
         <Route path='/search' element={<Search/>} />
         <Route path='/cart' element={<Cart/>} />
+
+
+        {/* Admin routes */}
+        // Rotues
+
+<Route
+//   element={
+//     <ProtectedRoute isAuthenticated={true} adminRoute={true} isAdmin={true} />
+//   }
+>
+  <Route path="/admin/dashboard" element={<Dashboard />} />
+  <Route path="/admin/product" element={<Products />} />
+  <Route path="/admin/customer" element={<Customers />} />
+  <Route path="/admin/transaction" element={<Transaction />} />
+  {/* Charts */}
+  <Route path="/admin/chart/bar" element={<Barcharts />} />
+  <Route path="/admin/chart/pie" element={<Piecharts />} />
+  <Route path="/admin/chart/line" element={<Linecharts />} />
+  {/* Apps */}
+  <Route path="/admin/app/coupon" element={<Coupon />} />
+  <Route path="/admin/app/stopwatch" element={<Stopwatch />} />
+  <Route path="/admin/app/toss" element={<Toss />} />
+
+  {/* Management */}
+  <Route path="/admin/product/new" element={<NewProduct />} />
+
+  <Route path="/admin/product/:id" element={<ProductManagement />} />
+
+  <Route path="/admin/transaction/:id" element={<TransactionManagement />} />
+</Route>;
       </Routes>
       </Suspense>
     </Router>
