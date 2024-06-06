ef880a81c6fa655fbf63604baccc2702e5df16a7
Sachin <raisethebat2020@gmail.com>
2024-06-07 01:40:39 +0530

    header created

--- ecommerce-frontend/src/App.tsx
+++ ecommerce-frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 import { lazy,Suspense } from 'react'
 import Loader from './components/loader';
+import Header from './components/header';
 
 const Home = lazy(()=> import('./pages/home'));
 const Search = lazy(()=> import('./pages/search'));
@@ -30,6 +31,7 @@ const TransactionManagement = lazy(
 const App = () => {
   return (
     <Router>
+      <Header/>
       <Suspense fallback={<Loader/>} >
       <Routes>
         <Route path='/' element={<Home/>} />
