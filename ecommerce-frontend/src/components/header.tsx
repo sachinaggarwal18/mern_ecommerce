30d898ad52464dfecc6fec73de9caf568d2ee3ba
Sachin <raisethebat2020@gmail.com>
2024-06-12 11:08:46 +0530

    added search page

--- ecommerce-frontend/src/components/header.tsx
+++ ecommerce-frontend/src/components/header.tsx
@@ -3,7 +3,7 @@ import { FaSearch, FaShoppingBag, FaSign, FaSignOutAlt, FaUser } from "react-ico
 import { Link } from "react-router-dom";
 
 // const user={_id:"gxcbuu",role:"admin"};
-const user={_id:"",role:""};
+const user={_id:"xhuhiu",role:""};
 
 const Header = () => {
 
--- ecommerce-frontend/src/pages/search.tsx
+++ ecommerce-frontend/src/pages/search.tsx
@@ -1,10 +1,96 @@
-import React from 'react'
+import { useState } from "react"
+import ProductCard from "../components/productCard";
 
 const search = () => {
+
+  const [search,setSearch] = useState("");
+  const [sort,setSort] = useState("");
+  const [maxPrice,setMaxPrice] = useState(10000);
+  const [category,setCategory] = useState("");
+  const [page,setPage] = useState(1);
+
+  const addToCartHandler = () => {};
+
+  const isPrevPage = page > 1;
+  const isNextPage = page < 4;
+
+
   return (
-    <div>
-     search items
-    </div>
+   <div className="product-search-page">
+    <aside>
+      <h2>Filters</h2>
+
+      <div>
+        <h4>Sort</h4>
+        <select 
+          value={sort}
+          onChange={(e) => setSort(e.target.value)}
+        >
+          <option value="">None</option>
+          <option value="asc">Price (Low to High)</option>
+          <option value="dsc">Price (High to Low)</option>
+        </select>
+      </div>
+
+
+      <div>
+        <h4>Max Price: {maxPrice || ""}</h4>
+        <input 
+          type="range"
+          min={100}
+          max={100000}
+          value={sort}
+          onChange={(e) => setMaxPrice(Number(e.target.value))}
+        />
+      </div>
+
+      <div>
+        <h4>Category</h4>
+        <select 
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option value="">None</option>
+          <option value="">Sample1</option>
+          <option value="">Sample1</option>
+        </select>
+      </div>
+</aside>
+
+    <main>
+
+      <h1>Products</h1>
+      <input type="text" placeholder="Search by name..." value={search} onChange={(e) => setSearch(e.target.value)} />
+
+      <div className="search_product-list">
+      <ProductCard
+          productId="abjhuybiu"
+          name="Macbook"
+          price={80000}
+          stock={10} 
+          handler={addToCartHandler} 
+          photo="https://stadt-bremerhaven.de/wp-content/uploads/2022/12/macbook.jpg"
+        />
+      </div>
+
+      <article>
+
+        <button
+          disabled={!isPrevPage}
+          onClick={() => setPage((prev) => prev - 1)}
+        >Prev</button>
+
+        <span>{page} of 4</span>
+
+        <button 
+          disabled={!isNextPage}
+          onClick={() => setPage((prev) => prev + 1)}>Next</button>
+
+      </article>
+
+    </main>
+
+   </div>
   )
 }
 
